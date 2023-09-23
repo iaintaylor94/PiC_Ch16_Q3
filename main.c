398551edc661451abebf065af3516c2f39108224
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-23 13:21:05 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
+// program to copy a file to another file while capitalising each letter
+
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int c;
+  char inName[64], outName[64];
+  FILE *in, *out;
+  
+  // Get file names from the user
+  printf ("Enter file name to copy from: ");
+  scanf ("%63s", inName);
+  printf ("Enter file name to copy to: ");
+  scanf ("%63s", outName);
+
+  // Open input and output files
+  if ( (in = fopen (inName, "r")) == NULL ) {
+    printf ("Can't open %s for reading.\n", inName);
+    return 1;
+  }
+
+  if ( (out = fopen (outName, "w")) == NULL ) {
+    printf ("Can't open %s for writing.\n", outName);
+    return 1;
+  } 
+
+  // Copy in to out
+  while ( (c = getc (in)) != EOF ) {
+    c = toupper(c);
+    putc (c, out);
+  }
+
+  // Close files
+  fclose (in);
+  fclose (out);
+
+  printf ("File has been copied\n");
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Programming in C
+## Chapter 16
+### Question 3
+
+This problem is from chapter 16 of the textbook[^1]. This program copies the input from one file to another capitalising each letter before moving it.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 371 : Question 3
